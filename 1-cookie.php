1d0756f1c8784fece4b666da5d4eb254e3d8358f
Your Name <you@example.com>
2019-01-11 19:11:14 +0700

    another change

---
+++ 1-cookie.php
@@ -0,0 +1,27 @@
+<?php
+echo "<a href=\"./\">BACK</a>";
+/* 1. --- COOKIE ---
+ *
+ * setcookie(name,value,expire,path,domain,secure,httponly);
+ *
+ * setcookie must be set in top of file.
+ *
+ * NOTE : after setcookie, should be refresh page
+ */
+
+echo "<h1>1. COOKIE</h1>";
+
+
+
+echo "<br><br>DO EXERCISE INSIDE COMMENT CODE BELOW THIS LINE<hr>";
+$cookie_name="user";
+$cookie_value="Nguyen Hai Nam";
+setcookie($cookie_name,$cookie_value,time()+(24*3600));
+if(isset($_COOKIE[$cookie_name]))
+{
+	echo "your cookie is set up";
+}
+	
+
+
+?>
